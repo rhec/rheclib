b08fdc486d60c0f701ce758a7b27d7b1cd5ee063
Robert Eanes <github@robsinbox.com>
2009-11-04 16:05:47 -0500

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
  
 setup(
     name='rheclib',
-    version='0.1',
+    version='0.1.1',
     description='Grab bag django app',
     author='Robert Eanes',
     author_email='django@robsinbox.com',
