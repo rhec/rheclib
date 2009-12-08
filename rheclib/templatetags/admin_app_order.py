4f93a6edd7b1f05554bf096e1de89745d54d5cb3
Robert Eanes <github@robsinbox.com>
2009-12-07 23:44:06 -0500

    Docstring change

--- rheclib/templatetags/admin_app_order.py
+++ rheclib/templatetags/admin_app_order.py
@@ -6,8 +6,14 @@ register = template.Library()
 def reorder_admin_apps(app_list):
     """
     This will reorder the apps in the admin using weights defined in the RHEC_ADMIN_APP_WEIGHTS dict.
-    The names, unfortunately, must be the verbose names, not the actual app name, because that is what is in the admin app_list var.
+    The names, unfortunately, must be the verbose names displayed in the admin, not the actual app name, 
+    because that is what is in the admin app_list var.
     The app_list argument object is modified, it does not return a value.
+    Usage: Define your app weights in settings.py like so:
+    RHEC_ADMIN_APP_WEIGHTS = {'Sites': 1,
+                              'Auth' : 2}
+    Override the default admin index.html template and insert the following before the app_list is rendered:
+    {% load admin_app_order %}{% reorder_admin_apps app_list %}
     """
     weights = getattr(settings, 'RHEC_ADMIN_APP_WEIGHTS', {})
     for app_dict in app_list:
